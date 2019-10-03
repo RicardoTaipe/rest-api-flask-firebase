ce4a86c72f8b51585f6df66b9b64d840fc889c73
RicardoTaipe <rdavid28@hotmail.es>
2019-10-02 20:58:32 -0500

    flask rest api

--- app.py
+++ app.py
@@ -1,49 +1,58 @@
+import firebase_admin
+from firebase_admin import credentials, db
+import flask
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('./service-account-key.json')
 
-    # For debugging
-    print(f"got name {name}")
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://<db-name>.firebaseio.com'
+})
 
-    response = {}
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+CARS = db.reference('cars')
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+@app.route('/cars', methods=['GET','POST'])
+def create_or_get_cars():
+    if request.method == 'GET':
+        # Retrieve all cars
+        return jsonify( CARS.get()), 200
+    if request.method == 'POST':
+        # Create a new car
+        req = request.json
+        car = CARS.push(req)
+        return jsonify({'id': car.key}), 201
 
-    # Return the response in json format
-    return jsonify(response)
+# retrieve info about a specific car by id
+@app.route('/cars/<id>', methods=['GET'])
+def read_car(id):
+    return jsonify(_ensure_car(id))
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+#update a car
+@app.route('/cars/<id>', methods=['PUT'])
+def update_car(id):
+    _ensure_car(id)
+    req = request.json
+    CARS.child(id).update(req)
+    return jsonify({'info': 'car updated'})
 
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+#delete a car
+@app.route('/cars/<id>', methods=['DELETE'])
+def delete_car(id):
+    _ensure_car(id)
+    CARS.child(id).delete()
+    return jsonify({'info':'Car deleted'})
+
+# verify that exist a car's id
+def _ensure_car(id):
+    car = CARS.child(id).get()
+    if not car:
+        flask.abort(404)
+    return car
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
